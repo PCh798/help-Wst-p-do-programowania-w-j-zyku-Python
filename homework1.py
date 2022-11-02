0c8946194d4552abaa0cc61dff3bc5d1373b874d
PCh798 <79383827+PCh798@users.noreply.github.com>
2022-11-02 18:26:17 +0100

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,9 @@
+home = ["dishes", "laundry", "dinner", "shopping", "vacuum"]
+
+work = ["emails", "reaserch", "notes", "meeting", "reading"]
+to_do_list = [home, work]
+
+for homework in to_do_list:
+    for elem in homework:
+        print(elem)
+
